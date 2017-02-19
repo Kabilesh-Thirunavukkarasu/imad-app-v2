b4dc8305d54690d2de58094f15696b572df6e298
Kabilesh-Thirunavukkarasu <kabilesh.19cs@licet.ac.in>
2017-02-19 17:50:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ var htmlTemplate=`
 }
 
 app.get('/:articleName',function(req,res) {
-    var articleName=req.params.articleName;
+    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
 

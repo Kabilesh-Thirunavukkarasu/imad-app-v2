1045270f1097c86be905f4c619b02406d0e27fa0
Kabilesh-Thirunavukkarasu <kabilesh.19cs@licet.ac.in>
2017-03-04 10:11:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('kb');
 element.innerHTML='This is the text';
 
-var imp=document.getElementById('pic');
+var img=document.getElementById('pic');
 
 var marginLeft = 0;
 function moveRight(){
     marginLeft=marginLeft+10;
-    pic.style.marginLeft=marginLeft + 'px';
+    img.style.marginLeft=marginLeft + 'px';
 }
 pic.onclick = function() {
     var interval = setinterval(moveRight,100);

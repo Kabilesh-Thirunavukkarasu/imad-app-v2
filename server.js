1e2d1689c3f1f3ac3c65a2934a9c5b8047e55380
Kabilesh-Thirunavukkarasu <kabilesh.19cs@licet.ac.in>
2017-03-04 09:31:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/5E8B2160-2.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '5E8B2160-2.jpg'));
 });

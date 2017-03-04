ce3c32090d144df701c9461933786089356c8235
Kabilesh-Thirunavukkarasu <kabilesh.19cs@licet.ac.in>
2017-03-04 09:57:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('kb');
 element.innerHTML='This is the text';
+
+var imp=document.getElementById('img');
+
+img.onclick = function() {
+    img.style.marginLeft='100px';
+};

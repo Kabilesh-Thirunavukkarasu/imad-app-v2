7a517d1f47084c269823eca1907c6505a737805e
Kabilesh-Thirunavukkarasu <kabilesh.19cs@licet.ac.in>
2017-03-04 10:15:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,9 @@ var img=document.getElementById('pic');
 
 var marginLeft = 0;
 function moveRight(){
-    marginLeft=marginLeft+5;
+    marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft + 'px';
 }
 pic.onclick = function() {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
 };
